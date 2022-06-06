c6d6a6ce6ba7a07a31c7772950adc19b00aacec2
dyk010518 <dyk010518@naver.com>
2022-06-06 13:42:14 -0400

    favicon icon inserted and minor path error resolved

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: 'stock_analyzer',
-  assetPrefix: 'stock_analyzer',
+  basePath: '/stock_analyzer',
+  assetPrefix: '/stock_analyzer',
 }
 
 module.exports = nextConfig
