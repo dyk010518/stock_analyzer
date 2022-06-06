8335bc617a99aa83f165a6bfaedbfbf56f61f018
dyk010518 <dyk010518@naver.com>
2022-06-06 14:34:14 -0400

    customized apis

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -16,39 +16,10 @@ const article = ({article}) => {
     </>
 }
 
-// export const getStaticProps = async (context) => {
-//     const res = await fetch(`${server}/api/articles/${context.params.id}`)
-
-//     const article = await res.json()
-
-//     return {
-//         props: {
-//             article
-//         }
-//     }
-
-// }
-
-// export const getStaticPaths = async () => {
-//     const res = await fetch(`${server}/api/articles/`)
-
-//     const articles = await res.json()
-
-//     const ids = articles.map(article => article.id)
-//     const paths = ids.map(id => ({params: {id: id.toString()}}))
-
-//     return {
-//         paths,
-//         fallback: false,
-//     }
-// }
-
 export const getStaticProps = async (context) => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const res = await fetch(`${server}/api/articles/${context.params.id}`)
 
     const article = await res.json()
-
-    console.log(article.id)
 
     return {
         props: {
@@ -59,7 +30,7 @@ export const getStaticProps = async (context) => {
 }
 
 export const getStaticPaths = async () => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const res = await fetch(`${server}/api/articles/`)
 
     const articles = await res.json()
 
@@ -71,5 +42,34 @@ export const getStaticPaths = async () => {
         fallback: false,
     }
 }
+
+// export const getStaticProps = async (context) => {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+
+//     const article = await res.json()
+
+//     console.log(article.id)
+
+//     return {
+//         props: {
+//             article
+//         }
+//     }
+
+// }
+
+// export const getStaticPaths = async () => {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+//     const articles = await res.json()
+
+//     const ids = articles.map(article => article.id)
+//     const paths = ids.map(id => ({params: {id: id.toString()}}))
+
+//     return {
+//         paths,
+//         fallback: false,
+//     }
+// }
 
 export default article
