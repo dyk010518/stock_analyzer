9d3a86034cf43d1278cae4f283cb24960d13f2bf
dyk010518 <dyk010518@naver.com>
2022-06-08 01:12:37 -0400

    Basic stock components set up

---
+++ components/PFCFRatio.js
@@ -0,0 +1,25 @@
+import stockStatsStyles from '../styles/StockStats.module.css'
+
+const PFCFRatio = () => {
+    return (
+        <thead>
+            <tr className={stockStatsStyles.row}>
+                <th className={stockStatsStyles.col}>
+                    <p1>Terminal P/FCF</p1>
+                </th>
+                <th className={stockStatsStyles.col}>
+                    <p2>1 year</p2>
+                    <p2>5 year</p2>
+                    <p2>10 year</p2>
+                </th>
+                <th className={stockStatsStyles.col}>
+                    <div><input type="number" id="quantity" name="quantity" min="1" max="5" /></div>
+                    <div><input type="number" id="quantity" name="quantity" min="1" max="5" /></div>
+                    <div><input type="number" id="quantity" name="quantity" min="1" max="5" /></div>
+                </th>
+            </tr>
+        </thead>
+    )
+}
+
+export default PFCFRatio
--- components/StockAnalysis.js
+++ components/StockAnalysis.js
@@ -1,11 +1,25 @@
 import Link from 'next/link'
 import stockStyles from '../styles/StockAnalysis.module.css'
 import TopRow from './TopRow'
+import Revenue from './Revenue'
+import ProfitMargin from './ProfitMargin'
+import FCFMargin from './FCFMargin'
+import PERatio from './PERatio'
+import PFCFRatio from './PFCFRatio'
+import AnnualReturn from './AnnualReturn'
+
+
 
 const StockAnalysis = () => {
     return (
         <table className={stockStyles.table}>
             <TopRow />
+            <Revenue />
+            <ProfitMargin />
+            <FCFMargin />
+            <PERatio />
+            <PFCFRatio />
+            <AnnualReturn />
         </table>
     )
 }
