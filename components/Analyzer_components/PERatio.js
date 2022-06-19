82f3d59ad83a5957de11b18c23cf3123cf356787
dyk010518 <dyk010518@naver.com>
2022-06-19 19:32:49 -0400

    more info shown

--- components/Analyzer_components/PERatio.js
+++ components/Analyzer_components/PERatio.js
@@ -1,16 +1,19 @@
 import stockStatsStyles from '../../styles/StockStats.module.css'
+import { getAverage } from '../StockAnalyzer'
 
-const PERatio = () => {
+const PERatio = ({reports}) => {
+    const PERatio = getPERatio(reports.IS, reports.BS, reports.DA)
+
     return (
         <thead>
             <tr className={stockStatsStyles.row}>
                 <th className={stockStatsStyles.col}>
-                    <p1>Terminal P/E</p1>
+                    <p1>P/E Ratio</p1>
                 </th>
                 <th className={stockStatsStyles.col}>
-                    <p2>1 year</p2>
-                    <p2>5 year</p2>
-                    <p2>10 year</p2>
+                    <p2>{PERatio}</p2>
+                    <p2>-</p2>
+                    <p2>-</p2>
                 </th>
                 <th className={stockStatsStyles.col}>
                     <div><input type="number" id="quantity" name="quantity" min="1" max="5" /></div>
@@ -20,6 +23,27 @@ const PERatio = () => {
             </tr>
         </thead>
     )
+}
+
+const getPERatio = (IS, BS, DA) => {
+    const number_of_quarters = IS.quarterlyReports.length
+    if(number_of_quarters == 0) return "NA"
+
+    const earning = getAnnualEarning(IS)
+    const shares = BS.quarterlyReports[0].commonStockSharesOutstanding != "None" ? Number(BS.quarterlyReports[0].commonStockSharesOutstanding) : undefined
+    const sharePrice = Number(DA["Time Series (Daily)"][Object.keys(DA["Time Series (Daily)"])[0]]["5. adjusted close"])
+
+    return earning != 0 && shares ? (sharePrice*shares/earning).toFixed(2) : "NA"
+}
+
+
+const getAnnualEarning = (IS) => {
+    const start_quarter = Math.min(4, IS.quarterlyReports.length)
+    let earnings = 0
+    for(var quarter = 0; quarter < start_quarter; quarter ++){
+        if(IS.quarterlyReports[quarter].netIncome != "None") earnings = earnings + Number(IS.quarterlyReports[quarter].netIncome)
+    }
+    return earnings
 }
 
 export default PERatio
--- components/SymbolSearch.js
+++ components/SymbolSearch.js
@@ -1,12 +1,24 @@
 import searchStyles from '../styles/SymbolSearch.module.css'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const SymbolSearch = () => {
+    const router = useRouter()
+
+    const handleClick = (input) => {
+        input && router.push(`/analysis/${input.value.toUpperCase()}`)
+    }
+
     return (
         <div className={searchStyles.container}>
-            <input type='text' placeholder='Search Stock Symbol' className={searchStyles.search}></input>
-            <button className={searchStyles.button}><img src="/search.png" className={searchStyles.glass}></img></button>
+            <input type='text' id="userInput" placeholder='Search Stock Symbol' className={searchStyles.search}></input>
+            {typeof window !=='undefined' && (document.getElementById("userInput").value = "")}
+            <button className={searchStyles.button} onClick={() => handleClick(document.getElementById("userInput"))}><img src="/search.png" className={searchStyles.glass}></img></button>
         </div>
     )
 }
+
+
+
 
 export default SymbolSearch
