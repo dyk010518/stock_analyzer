9b3c02c390aa82fec53b3595eaa15d9870d188e8
dyk010518 <dyk010518@naver.com>
2022-06-20 23:02:26 -0400

    Company name shown

--- pages/analysis/[symbol]/index.js
+++ pages/analysis/[symbol]/index.js
@@ -6,17 +6,19 @@ import { StockAnalyzer } from "../../../components/StockAnalyzer"
 import AnalyzeResult from '../../../components/AnalyzeResult'
 
 
-const article = ({income_statement, balance_sheet, cash_flow, daily_adjusted}) => {
+const article = ({income_statement, balance_sheet, cash_flow, daily_adjusted, stock_info}) => {
     const reports = {
         IS: income_statement,
         BS: balance_sheet,
         CF: cash_flow,
         DA: daily_adjusted,
+        SI: stock_info,
     }
     const found = income_statement.symbol ? true : false  
 
     return <>
         <Meta />
+        {found && <h1>{reports.SI.Name}</h1>}
         {found && <h1>{reports.IS.symbol}</h1>}
         <br />
         {found && <StockAnalyzer reports={reports}/>}
@@ -31,7 +33,6 @@ export const getServerSideProps = async (context) => {
     const res1 = await fetch(`https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=${context.params.symbol}&apikey=${process.env.ALPHA_API_KEY}`)
     const income_statement = await res1.json()
 
-
     const res2 = await fetch(`https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=${context.params.symbol}&apikey=${process.env.ALPHA_API_KEY}`)
     const balance_sheet = await res2.json()
 
@@ -41,43 +42,25 @@ export const getServerSideProps = async (context) => {
     const res4 = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${context.params.symbol}&outputsize=full&apikey=${process.env.ALPHA_API_KEY}`)
     const daily_adjusted = await res4.json() 
 
+    const res5 = await fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${context.params.symbol}&apikey=${process.env.ALPHA_API_KEY}`)
+    const stock_info = await res5.json()
+
     return {
         props: {
             income_statement, 
             balance_sheet,
             cash_flow,
             daily_adjusted,
+            stock_info,
         }
     }
 }
 
-// export const getStaticProps = async (context) => {
-//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
-
-//     const article = await res.json()
-
-//     console.log(article.id)
-
-//     return {
-//         props: {
-//             article
-//         }
-//     }
-
-// }
-
-// export const getStaticPaths = async () => {
-//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
-
-//     const articles = await res.json()
-
-//     const ids = articles.map(article => article.id)
-//     const paths = ids.map(id => ({params: {id: id.toString()}}))
-
-//     return {
-//         paths,
-//         fallback: false,
-//     }
-// }
+const getCompanyStats = async (symbol) => {
+    const res = await fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}}&apikey=${process.env.ALPHA_API_KEY}`)
+    const obj = await res.json()
+    console.log(obj)
+    return 1
+}
 
 export default article
