fa738156d8fc4051af560280c5cf5783ab94767b
dyk010518 <dyk010518@naver.com>
2022-06-22 23:18:31 -0400

    currency added

--- components/StockTitle.js
+++ components/StockTitle.js
@@ -4,6 +4,7 @@ const StockTitle = ({info}) => {
     const symbol = info.SI["Symbol"]
     const name = info.SI["Name"]
     const exchange = info.SI["Exchange"]
+    const currency = info.SI["Currency"]
     const current_price = Number(info.PI["c"]).toFixed(2)
     const difference = Number(info.PI["d"]).toFixed(2)
     const difference_percent = Number(info.PI["dp"]).toFixed(2)
@@ -11,7 +12,7 @@ const StockTitle = ({info}) => {
     return (
         <>
             <h2>{name} ({symbol})</h2>
-            <div>{exchange}</div>
+            <div>{exchange} | Currency in {currency}</div>
             <h3>{current_price}</h3>
             {difference >= 0 && <div className={stockTitleStyle.positive}>{difference} ({difference_percent}%)</div>}
             {difference < 0 && <div className={stockTitleStyle.negative}>{difference} ({difference_percent}%)</div>}
