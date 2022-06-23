81cb922aeffc67e7d84ba5964bcfe328f897087a
dyk010518 <dyk010518@naver.com>
2022-06-22 23:18:31 -0400

    currency added

--- .next/static/webpack/webpack.0806b65cf6681d6a.hot-update.js
+++ .next/static/webpack/webpack.0806b65cf6681d6a.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "6eae5c348d7614c9"; }
+/******/ 	__webpack_require__.h = function() { return "d44cafa57d01d11f"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
