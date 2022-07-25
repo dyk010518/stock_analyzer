abb8e2c02df4c1189809b0b343eb345d2b955d1f
dyk010518 <dyk010518@naver.com>
2022-07-24 23:18:22 -0400

    basic instruction added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a98eed00e82b4565"; }
+/******/ 		__webpack_require__.h = function() { return "1769d99a21aa031d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
