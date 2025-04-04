e7cee226ef27c8bd37576d325ce2005008e13cf3
Daniel Kim <danielkim@secondspectrum.com>
2025-04-04 18:08:08 -0400

    feat: Update to 3 and 5 years

--- components/Analyzer_components/Revenue.js
+++ components/Analyzer_components/Revenue.js
@@ -29,13 +29,13 @@ const getRevenueNumbers = (IS) => {
     const number_of_quarters = IS.quarterlyReports.length
     const year_one = number_of_quarters >= 4 ? getAverage([getQuarterlyRevenue(IS, 0), getQuarterlyRevenue(IS, 1), getQuarterlyRevenue(IS, 2), getQuarterlyRevenue(IS, 3)])*4 : undefined
     const year_two = number_of_quarters >= 8 ? getAverage([getQuarterlyRevenue(IS, 4), getQuarterlyRevenue(IS, 5), getQuarterlyRevenue(IS, 6), getQuarterlyRevenue(IS, 7)])*4 : undefined
-    const year_three = number_of_quarters >= 12 ? getAverage([getQuarterlyRevenue(IS, 8), getQuarterlyRevenue(IS, 9), getQuarterlyRevenue(IS, 10), getQuarterlyRevenue(IS, 11)])*4 : undefined
-    const year_five = number_of_quarters >= 20 ? getAverage([getQuarterlyRevenue(IS, 16), getQuarterlyRevenue(IS, 17), getQuarterlyRevenue(IS, 18), getQuarterlyRevenue(IS, 19)])*4 : undefined
+    const year_four = number_of_quarters >= 16 ? getAverage([getQuarterlyRevenue(IS, 12), getQuarterlyRevenue(IS, 12), getQuarterlyRevenue(IS, 14), getQuarterlyRevenue(IS, 15)])*4 : undefined
+    const year_six = number_of_quarters >= 24 ? getAverage([getQuarterlyRevenue(IS, 20), getQuarterlyRevenue(IS, 21), getQuarterlyRevenue(IS, 22), getQuarterlyRevenue(IS, 23)])*4 : undefined
 
     const growth_one = year_one && year_two ? ((year_one/year_two - 1)*100).toFixed(2) : "N/A"
-    const growth_two = year_one && year_three ? (((year_one/year_three)**(1/2) - 1)*100).toFixed(2) : "N/A"
-    const growth_four = year_one && year_five ? (((year_one/year_five)**(1/4) - 1)*100).toFixed(2) : "N/A"
-    return [growth_one, growth_two, growth_four]
+    const growth_three = year_one && year_four ? (((year_one/year_four)**(1/3) - 1)*100).toFixed(2) : "N/A"
+    const growth_five = year_one && year_six ? (((year_one/year_six)**(1/5) - 1)*100).toFixed(2) : "N/A"
+    return [growth_one, growth_three, growth_five]
 }
 
 const getQuarterlyRevenue = (IS, quarter) => {
