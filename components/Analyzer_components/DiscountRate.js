4fc2976146795220b071a3ac56cbdb0d6f37a032
Daniel Kim <danielkim@secondspectrum.com>
2025-04-04 17:58:44 -0400

    feat: Change Annual Return to Discount Rate

--- components/Analyzer_components/DiscountRate.js
+++ components/Analyzer_components/DiscountRate.js
@@ -1,11 +1,11 @@
 import stockStatsStyles from '../../styles/StockStats.module.css'
 
-const AnnualReturn = () => {
+const DiscountRate = () => {
     return (
         <thead>
             <tr className={stockStatsStyles.row}>
                 <th className={stockStatsStyles.col1}>
-                    <p1>Annual Return (%)</p1>
+                    <p1>Discount Rate (%)</p1>
                 </th>
                 <th className={stockStatsStyles.col1}>
                     <p2>-</p2>
@@ -13,13 +13,13 @@ const AnnualReturn = () => {
                     <p2>-</p2>
                 </th>
                 <th className={stockStatsStyles.col1}>
-                    <div><input type="number" id="AnnualReturn_input_low" name="quantity" />  %</div>
-                    <div><input type="number" id="AnnualReturn_input_mid" name="quantity" />  %</div>
-                    <div><input type="number" id="AnnualReturn_input_high" name="quantity" />  %</div>
+                    <div><input type="number" id="DiscountRate_input_low" name="quantity" />  %</div>
+                    <div><input type="number" id="DiscountRate_input_mid" name="quantity" />  %</div>
+                    <div><input type="number" id="DiscountRate_input_high" name="quantity" />  %</div>
                 </th>
             </tr>
         </thead>
     )
 }
 
-export default AnnualReturn
+export default DiscountRate
