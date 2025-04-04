2c0b2a4a9f4c1d070861d1f952ce79ef351aafad
Daniel Kim <danielkim@secondspectrum.com>
2025-04-04 17:58:44 -0400

    feat: Change Annual Return to Discount Rate

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "40dead0f05955ea0"; }
+/******/ 		__webpack_require__.h = function() { return "dd24d32ef335a6e1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
