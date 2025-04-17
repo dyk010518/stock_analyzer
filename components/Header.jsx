56af29fac7ac668f0caee9134a788c14774d22ad
Daniel Kim <danielkim@secondspectrum.com>
2025-04-17 13:31:05 -0400

    feat: Removed Header css

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,21 @@
-import headerStyles from '../styles/Header.module.css'
-
 const Header = () => {
     return (
         <div>
-            <h1 className = {headerStyles.title}>
-                <span>Stock</span> Analyzer
+            <h1
+                className="text-[4rem] mb-16 leading-[1.15] text-[#aaa] text-center"
+                style={{
+                    textShadow: '1px 0 0 #aaa, 0 -1px 0 #aaa, 0 1px 0 #aaa, -1px 0 0 #aaa',
+                }}
+            >
+                <span
+                    className="text-[#d5a82a]"
+                    style={{
+                        textShadow: '1px 0 0 #aaa, 0 -1px 0 #aaa, 0 1px 0 #aaa, -1px 0 0 #aaa',
+                    }}
+                >
+                    Stock
+                </span>{' '}
+                Analyzer
             </h1>
         </div>
     )
