7b63f162e51514bdc467bf75397aca0a5b166d5f
Daniel Kim <danielkim@secondspectrum.com>
2025-04-18 13:07:17 -0400

    feat: Move finance functions to lib/

--- components/Analyzer_components/PERatio.jsx
+++ components/Analyzer_components/PERatio.jsx
@@ -1,4 +1,5 @@
 import stockStatsStyles from '../../styles/StockStats.module.css'
+import { getPERatio } from '../../lib/finance'
 
 const PERatio = (props) => {
     const PERatio = getPERatio(props.reports.IS, props.reports.BS, props.reports.PI)
@@ -22,26 +23,6 @@ const PERatio = (props) => {
             </tr>
         </thead>
     )
-}
-
-const getPERatio = (IS, BS, PI) => {
-    const number_of_quarters = IS.quarterlyReports.length
-    if(number_of_quarters == 0) return "NA"
-
-    const earning = getAnnualEarning(IS)
-    const shares = BS.quarterlyReports[0].commonStockSharesOutstanding != "None" ? Number(BS.quarterlyReports[0].commonStockSharesOutstanding) : undefined
-    const sharePrice = Number(PI["c"])
-    return earning != 0 && shares ? (sharePrice*shares/earning).toFixed(2) : "NA"
-}
-
-
-const getAnnualEarning = (IS) => {
-    const start_quarter = Math.min(4, IS.quarterlyReports.length)
-    let earnings = 0
-    for(var quarter = 0; quarter < start_quarter; quarter ++){
-        if(IS.quarterlyReports[quarter].netIncome != "None") earnings = earnings + Number(IS.quarterlyReports[quarter].netIncome)
-    }
-    return earnings
 }
 
 export default PERatio
--- components/Analyzer_components/Revenue.jsx
+++ components/Analyzer_components/Revenue.jsx
@@ -1,5 +1,5 @@
 import stockStatsStyles from '../../styles/StockStats.module.css'
-import { getAverage } from '../../utils/utils'
+import { getRevenueNumbers } from '../../lib/finance'
 
 const Revenue = (props) => {
     const revenueGrowth = getRevenueNumbers(props.reports.IS)
@@ -23,23 +23,6 @@ const Revenue = (props) => {
             </tr>
         </thead>
     )
-}
-
-const getRevenueNumbers = (IS) => {
-    const number_of_quarters = IS.quarterlyReports.length
-    const year_one = number_of_quarters >= 4 ? getAverage([getQuarterlyRevenue(IS, 0), getQuarterlyRevenue(IS, 1), getQuarterlyRevenue(IS, 2), getQuarterlyRevenue(IS, 3)])*4 : undefined
-    const year_two = number_of_quarters >= 8 ? getAverage([getQuarterlyRevenue(IS, 4), getQuarterlyRevenue(IS, 5), getQuarterlyRevenue(IS, 6), getQuarterlyRevenue(IS, 7)])*4 : undefined
-    const year_four = number_of_quarters >= 16 ? getAverage([getQuarterlyRevenue(IS, 12), getQuarterlyRevenue(IS, 12), getQuarterlyRevenue(IS, 14), getQuarterlyRevenue(IS, 15)])*4 : undefined
-    const year_six = number_of_quarters >= 24 ? getAverage([getQuarterlyRevenue(IS, 20), getQuarterlyRevenue(IS, 21), getQuarterlyRevenue(IS, 22), getQuarterlyRevenue(IS, 23)])*4 : undefined
-
-    const growth_one = year_one && year_two ? ((year_one/year_two - 1)*100).toFixed(2) : "N/A"
-    const growth_three = year_one && year_four ? (((year_one/year_four)**(1/3) - 1)*100).toFixed(2) : "N/A"
-    const growth_five = year_one && year_six ? (((year_one/year_six)**(1/5) - 1)*100).toFixed(2) : "N/A"
-    return [growth_one, growth_three, growth_five]
-}
-
-const getQuarterlyRevenue = (IS, quarter) => {
-    return IS.quarterlyReports[quarter].totalRevenue != "None" ? Number(IS.quarterlyReports[quarter].totalRevenue) : undefined
 }
 
 export default Revenue
