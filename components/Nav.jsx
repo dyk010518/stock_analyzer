f3fbb07ff3a7f87e37e4aef062e67db35627d4b8
Daniel Kim <danielkim@secondspectrum.com>
2025-04-21 13:48:59 -0400

    feat: Google login logic

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,22 +1,45 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 const Nav = () => {
-    return (
-        <nav className="fixed top-0 left-0 w-full h-16 px-16 bg-[#021222] bg-opacity-80 text-white border-b-[0.5px] border-white z-20 flex justify-start items-center">
-            <ul className="flex flex-row items-center list-none space-x-8">
-                <li>
-                    <Link href='/'>
-                        Stock Analyzer
-                    </Link>
-                </li>
-                {/* <li>
-                    <Link href='/guide'>
-                        Guide
-                    </Link>
-                </li> */}
-            </ul>
-        </nav>
-    )
-}
+  const { data: session } = useSession();
 
-export default Nav
+  return (
+    <nav className="fixed top-0 left-0 w-full h-16 px-16 bg-[#021222] bg-opacity-80 text-white border-b-[0.5px] border-white z-20 flex justify-start items-center">
+      <ul className="flex flex-row items-center list-none space-x-8">
+        <li>
+          <Link href='/'>
+            Stock Analyzer
+          </Link>
+        </li>
+        {/* Add a conditional for sign-in/out */}
+        {session ? (
+          <>
+            <li className="text-sm">
+              Welcome, {session.user.name}
+            </li>
+            <li>
+              <button
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+                onClick={() => signOut()}
+              >
+                Sign out
+              </button>
+            </li>
+          </>
+        ) : (
+          <li>
+            <button
+              className="bg-blue-500 text-white px-4 py-2 rounded"
+              onClick={() => signIn('google')}
+            >
+              Sign in with Google
+            </button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
